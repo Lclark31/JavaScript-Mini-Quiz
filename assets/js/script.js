9952a474fcb7a3a25a9aa4c211c90caedabf29f8
Lashaun <clarklas831@gmail.com>
2021-06-25 22:44:26 -0500

    display user score

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,10 @@ let questionEl = document.querySelector(`.question`);
 let buttonsEl = document.querySelector(`.answer-btns`);
 let answerButtonEl = document.getElementsByClassName(`btns`);
 let headerEl = document.querySelector(`#ready`);
+let submitButtonEl = document.createElement(`button`);
+let timeLeft = 100;
+let questionNum = 0;
+let highscores = 0;
 
 let questions = [
   {
@@ -25,7 +29,7 @@ let questions = [
 ];
 
 function countdown() {
-  let timeLeft = 3;
+  timerEl.textContent = `You have ${timeLeft} seconds to finish`;
 
   let timeInterval = setInterval(() => {
     if (timeLeft > -1) {
@@ -33,25 +37,74 @@ function countdown() {
       timeLeft--;
     } else if (timeLeft <= 0) {
       clearInterval(timeInterval);
-      //   endQuiz();
     }
   }, 1000);
+  askNextQuestion();
 }
 
-// question with four buttons
+// create a input function so the user's initials are the key to the localStorage
+function userSignature() {
+  // console.log(score);
+  let inputEl = document.createElement(`input`);
+  inputEl.classList.add(`initials`);
 
-let questionNum = 0;
+  questionEl.textContent = `Enter your initials:`;
+  submitButtonEl.textContent = `Submit`;
+  submitButtonEl.classList.add(`submit-btn`);
+
+  document.querySelector(`.question`).appendChild(inputEl);
+  document.querySelector(`.container`).appendChild(submitButtonEl);
+
+  submitButtonEl.addEventListener(`click`, function () {
+    if (inputEl.value === ``) {
+      alert(`You must fill out the textbox!`);
+    } else {
+      localStorage.setItem(inputEl.value, score);
+      console.log(inputEl.value);
+    }
+  });
+}
+
+// userSignature();
 
 function endQuiz() {
-  console.log(`out of questions`);
+  timerEl.classList.add(`hide`);
+  buttonsEl.classList.add(`hide`);
+
+  score = timeLeft;
+  timeLeft = 0;
+  let highscore = localStorage.getItem(`highscore`);
+  if (highscore === null) {
+    localStorage.setItem(`highscore`, score);
+    questionEl.textContent = `New score of ${score}pts!`;
+  } else if (score > parseInt(highscore)) {
+    localStorage.setItem(`highscore`, score);
+    questionEl.textContent = `New Highscore! ${score}pts`;
+  } else {
+    questionEl.textContent = `You got a score of ${score}pts!`;
+  }
+
+  let acknowledgeButtonEl = document.createElement('button');
+  questionEl.appendChild(acknowledgeButtonEl);
+
+  acknowledgeButtonEl.classList.add(`submit-btn`);
+  acknowledgeButtonEl.textContent = `Submit Score!`;
+  acknowledgeButtonEl.addEventListener(`click`, userSignature);
 }
-// define startQuiz
+
+function saveScore() {
+  let highscore = localStorage.getItem(`highscores`);
+  if (score > parseInt(highscore)) {
+    localStorage.setItem(`highscore`, score);
+    highscore = localStorage.getItem(`highscore`);
+  }
+}
+
 function startQuiz() {
   countdown();
   startEl.classList.add(`hide`);
   headerEl.classList.add(`hide`);
   buttonsEl.classList.remove(`hide`);
-  askNextQuestion();
 }
 
 function askNextQuestion() {
@@ -72,22 +125,29 @@ function checkAnswer() {
   let correct = questions[questionNum].correctAnswer;
   let selectedButton = event.target;
   if (selectedButton.textContent === correct) {
+    document.querySelector(`.container`).classList.add(`correct`);
+    setInterval(() => {
+      document.querySelector(`.container`).classList.remove(`correct`);
+    }, 350);
     console.log(`nice`);
   } else {
     console.log(`wrong`);
+    document.querySelector(`.container`).classList.add(`wrong`);
+    setInterval(() => {
+      document.querySelector(`.container`).classList.remove(`wrong`);
+    }, 350);
+    timeLeft -= 20;
   }
   questionNum++;
-  askNextQuestion();
+  if (questions[questionNum] === undefined) {
+    endQuiz();
+  } else {
+    askNextQuestion();
+  }
 }
-// display correct or incorrect
-
-// subtract time if answer is incorrect
-
-// let the user save their score
 
 // display highscore from localStorage
 
-// define endQuiz()
 // view high scores in another html page
 
 // console.log(startEl);
